0d0945778804ff198aec2f702f34b87e2021c1aa
essu19 <33393654+essu19@users.noreply.github.com>
2017-11-06 20:29:53 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+import socket, time, sys
+
+def socket_create():
+    try:
+        global HOST
+        global PORT
+        global s
+        HOST = '192.168.0.199'
+        PORT = 9999
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("[*] Socket created.")
+    except:
+        print("[-] Socket creation error!")
+        time.sleep(2)
+        Main()
+
+def socket_connect():
+    try:
+        global HOST
+        global PORT
+        global s
+        s.connect((HOST, PORT))
+        print("[*] Connected to server {0}:{1}.".format(HOST, str(PORT)))
+    except:
+        print("[-] Connection failed!")
+        time.sleep(2)
+        socket_create()
+
+def loop():
+    while True:
+        cmd = input("> ")
+
+        if cmd == 'q' or cmd == 'quit':
+            s.send(str.encode(cmd))
+            s.close()
+            sys.exit()
+
+        elif len(cmd) > 0:
+            s.send(str.encode(cmd))
+            data = s.recv(1024)
+            print(data.decode("utf-8"))
+    s.close()
+
+def Main():
+    socket_create()
+    socket_connect()
+    loop()
+
+if __name__ == '__main__':
+    Main()
---
+++ server.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+import socket, time
+
+MAX_CLIENTS = 64
+CLIENTS = []
+
+def socket_create():
+    try:
+        global HOST
+        global PORT
+        global s
+        HOST = socket.gethostbyname(socket.gethostname())
+        PORT = 9999
+        s = socket.socket()
+        print("[*] Socket created.")
+    except:
+        print("[-] Socket creation error!")
+        time.sleep(2)
+        Main()
+
+def socket_bind():
+    try:
+        global HOST
+        global PORT
+        global s
+        s.bind((HOST, PORT))
+        s.listen(MAX_CLIENTS)
+        print("[*] Socket binded.")
+    except:
+        print("[-] Socket binding error: {0}".format(socket.error))
+        time.sleep(2)
+        socket_bind()
+
+def socket_accept():
+    for i in range(MAX_CLIENTS):
+        conn, addr = s.accept()
+        CLIENTS.append(addr)
+        print("User connected from {0}:{1}".format(str(addr[0]), str(addr[1])))
+        recv_message(conn)
+        conn.close()
+
+def recv_message(conn):
+    while True:
+        data = conn.recv(1024)
+        if len(data) > 0:
+            if data.decode("utf-8") == 'update':
+                conn.send(str.encode("Update file/folder huhu"))
+            elif data.decode("utf-8") == 'q' or data.decode("utf-8") == 'quit':
+                print("User disconnected!")
+            else:
+                conn.send(str.encode("Unknown command!"))
+    conn.close()
+
+def Main():
+    socket_create()
+    socket_bind()
+    socket_accept()
+
+if __name__ == '__main__':
+    Main()
